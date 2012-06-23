634887ffd9af097bd3a10522137eb0d62783a6d2
Hernan J Gonzalez <hgonzalez@gmail.com>
2012-06-23 09:19:38 -0300

    PngReader: end() method no longer deprecated. getRow() allows skipping rows

--- pnjg/src/ar/com/hjg/pngj/PngReader.java
+++ pnjg/src/ar/com/hjg/pngj/PngReader.java
@@ -14,6 +14,13 @@ import ar.com.hjg.pngj.chunks.PngMetadata;
 
 /**
  * Reads a PNG image, line by line
+ * <p>
+ * The reading sequence is as follows: <br>
+ * 1. At construction time, the header and IHDR chunk are read (basic image info) <br>
+ * 2. Optional: If you call GetMetadata() before reading the rows, the chunks before IDAT are automatically loaded <br>
+ * 3. The rows are read in strict sequence, from 0 to nrows-1 <br>
+ * 4. The reading of the last row triggers the loading of trailing chunks, and ends the reader.
+ * 
  */
 public class PngReader {
 	/**
@@ -26,29 +33,14 @@ public class PngReader {
 	 */
 	protected final String filename;
 
-	private int maxBytesChunksToLoad = 1024 * 1024; // for ancillary chunks
+	private ChunkLoadBehaviour chunkLoadBehaviour = ChunkLoadBehaviour.LOAD_CHUNK_ALWAYS; // see setter/getter
 
-	private ChunkLoadBehaviour chunkLoadBehaviour = ChunkLoadBehaviour.LOAD_CHUNK_ALWAYS;
+	private boolean shouldCloseStream = true; // true: closes stream after ending - see setter/getter
 
-	private final InputStream is;
-	private InflaterInputStream idatIstream;
-	private PngIDatChunkInputStream iIdatCstream;
+	private int maxBytesChunksToLoad = 1024 * 1024; // for ancillary chunks - see setter/getter
 
-	/**
-	 * Current chunk grounp, (0-6) already read or reading
-	 * <p>
-	 * see {@link ChunksList}
-	 */
-	protected int currentChunkGroup = -1;
-	/**
-	 * last read row number, starting from 0
-	 */
-	protected int rowNum = -1; // current row number
-	private int offset = 0;
-	private int bytesChunksLoaded; // bytes loaded from anciallary chunks
-
-	private final ChunksList chunksList;
-	private final PngMetadata metadata; // this a wrapper over chunks
+	protected final PngMetadata metadata; // this a wrapper over chunks
+	protected final ChunksList chunksList;
 
 	protected ImageLine imgLine;
 
@@ -57,7 +49,20 @@ public class PngReader {
 	protected byte[] rowbprev = null; // rowb previous
 	protected byte[] rowbfilter = null; // current line 'filtered': exactly as in uncompressed stream
 
-	private boolean shouldCloseStream = true; // true: closes stream after ending read
+	/**
+	 * Current chunk grounp, (0-6) already read or reading
+	 * <p>
+	 * see {@link ChunksList}
+	 */
+	protected int currentChunkGroup = -1;
+
+	protected int rowNum = -1; // last read row number, starting from 0
+	private int offset = 0; // offset in InputStream = bytes read
+	private int bytesChunksLoaded; // bytes loaded from anciallary chunks
+
+	protected final InputStream inputStream;
+	protected InflaterInputStream idatIstream;
+	protected PngIDatChunkInputStream iIdatCstream;
 
 	/**
 	 * Constructs a PngReader from an InputStream.
@@ -72,34 +77,40 @@ public class PngReader {
 	 */
 	public PngReader(InputStream inputStream, String filenameOrDescription) {
 		this.filename = filenameOrDescription == null ? "" : filenameOrDescription;
-		this.is = inputStream;
+		this.inputStream = inputStream;
 		this.chunksList = new ChunksList(null);
 		this.metadata = new PngMetadata(chunksList);
-		// reads header (magic bytes)
+		// starts reading: signature
 		byte[] pngid = new byte[PngHelperInternal.pngIdBytes.length];
-		PngHelperInternal.readBytes(is, pngid, 0, pngid.length);
+		PngHelperInternal.readBytes(inputStream, pngid, 0, pngid.length);
 		offset += pngid.length;
 		if (!Arrays.equals(pngid, PngHelperInternal.pngIdBytes))
 			throw new PngjInputException("Bad PNG signature");
 		// reads first chunk
 		currentChunkGroup = ChunksList.CHUNK_GROUP_0_IDHR;
-		int clen = PngHelperInternal.readInt4(is);
+		int clen = PngHelperInternal.readInt4(inputStream);
 		offset += 4;
 		if (clen != 13)
 			throw new RuntimeException("IDHR chunk len != 13 ?? " + clen);
 		byte[] chunkid = new byte[4];
-		PngHelperInternal.readBytes(is, chunkid, 0, 4);
+		PngHelperInternal.readBytes(inputStream, chunkid, 0, 4);
 		if (!Arrays.equals(chunkid, ChunkHelper.b_IHDR))
 			throw new PngjInputException("IHDR not found as first chunk??? [" + ChunkHelper.toString(chunkid) + "]");
 		offset += 4;
 		ChunkRaw chunk = new ChunkRaw(clen, chunkid, true);
-		offset += chunk.readChunkData(is);
-		PngChunkIHDR ihdr = (PngChunkIHDR) addChunkToList(chunk);
+		offset += chunk.readChunkData(inputStream);
+		PngChunkIHDR ihdr = (PngChunkIHDR) parseChunkAndAddToList(chunk);
 		boolean alpha = (ihdr.getColormodel() & 0x04) != 0;
 		boolean palette = (ihdr.getColormodel() & 0x01) != 0;
 		boolean grayscale = (ihdr.getColormodel() == 0 || ihdr.getColormodel() == 4);
+		// creates ImgInfo and imgLine, and allocates buffers
 		imgInfo = new ImageInfo(ihdr.getCols(), ihdr.getRows(), ihdr.getBitspc(), alpha, grayscale, palette);
 		imgLine = new ImageLine(imgInfo);
+		// allocation: one extra byte for filter type one pixel
+		rowbfilter = new byte[imgInfo.bytesPerRow + 1];
+		rowb = new byte[imgInfo.bytesPerRow + 1];
+		rowbprev = new byte[rowb.length];
+		// some checks
 		if (ihdr.getInterlaced() != 0)
 			throw new PngjUnsupportedException("PNG interlaced not supported by this library");
 		if (ihdr.getFilmeth() != 0 || ihdr.getCompmeth() != 0)
@@ -110,13 +121,9 @@ public class PngReader {
 		if (ihdr.getBitspc() != 1 && ihdr.getBitspc() != 2 && ihdr.getBitspc() != 4 && ihdr.getBitspc() != 8
 				&& ihdr.getBitspc() != 16)
 			throw new PngjInputException("Invalid bit depth " + ihdr.getBitspc());
-		// allocation: one extra byte for filter type one pixel
-		rowbfilter = new byte[imgInfo.bytesPerRow + 1];
-		rowb = new byte[imgInfo.bytesPerRow + 1];
-		rowbprev = new byte[rowb.length];
 	}
 
-	private PngChunk addChunkToList(ChunkRaw chunk) {
+	private PngChunk parseChunkAndAddToList(ChunkRaw chunk) {
 		// this requires that the currentChunkGroup is ok
 		PngChunk chunkType = PngChunk.factory(chunk, imgInfo);
 		if (!chunkType.crit) {
@@ -159,9 +166,20 @@ public class PngReader {
 		} catch (Exception e) {
 		}
 		readLastChunks();
-		if (shouldCloseStream) {
+		close();
+	}
+
+	private void close() {
+		if (currentChunkGroup < ChunksList.CHUNK_GROUP_6_END) { // this could only happen if forced close
 			try {
-				is.close();
+				idatIstream.close();
+			} catch (Exception e) {
+			}
+			currentChunkGroup = ChunksList.CHUNK_GROUP_6_END;
+		}
+		if (shouldCloseStream ) {
+			try {
+				inputStream.close();
 			} catch (Exception e) {
 				throw new PngjInputException("error closing input stream!", e);
 			}
@@ -184,11 +202,11 @@ public class PngReader {
 		while (!endfound) {
 			ignore = false;
 			if (!first) {
-				clen = PngHelperInternal.readInt4(is);
+				clen = PngHelperInternal.readInt4(inputStream);
 				offset += 4;
 				if (clen < 0)
 					throw new PngjInputException("bad len " + clen);
-				PngHelperInternal.readBytes(is, chunkid, 0, 4);
+				PngHelperInternal.readBytes(inputStream, chunkid, 0, 4);
 				offset += 4;
 			}
 			first = false;
@@ -202,9 +220,9 @@ public class PngReader {
 			ChunkRaw chunk = new ChunkRaw(clen, chunkid, true);
 			String chunkids = ChunkHelper.toString(chunkid);
 			boolean loadchunk = ChunkHelper.shouldLoad(chunkids, chunkLoadBehaviour);
-			offset += chunk.readChunkData(is);
+			offset += chunk.readChunkData(inputStream);
 			if (loadchunk && !ignore) {
-				addChunkToList(chunk);
+				parseChunkAndAddToList(chunk);
 			}
 		}
 		if (!endfound)
@@ -297,18 +315,18 @@ public class PngReader {
 		byte[] chunkid = new byte[4]; // it's important to reallocate in each iteration
 		currentChunkGroup = ChunksList.CHUNK_GROUP_1_AFTERIDHR;
 		while (!found) {
-			clen = PngHelperInternal.readInt4(is);
+			clen = PngHelperInternal.readInt4(inputStream);
 			offset += 4;
 			if (clen < 0)
 				break;
-			PngHelperInternal.readBytes(is, chunkid, 0, 4);
+			PngHelperInternal.readBytes(inputStream, chunkid, 0, 4);
 			offset += 4;
 			if (Arrays.equals(chunkid, ChunkHelper.b_IDAT)) {
 				found = true;
 				currentChunkGroup = ChunksList.CHUNK_GROUP_4_IDAT;
 				// add dummy idat chunk to list
 				ChunkRaw chunk = new ChunkRaw(0, chunkid, false);
-				addChunkToList(chunk);
+				parseChunkAndAddToList(chunk);
 				break;
 			} else if (Arrays.equals(chunkid, ChunkHelper.b_IEND)) {
 				throw new PngjInputException("END chunk found before image data (IDAT) at offset=" + offset);
@@ -316,18 +334,18 @@ public class PngReader {
 			ChunkRaw chunk = new ChunkRaw(clen, chunkid, true);
 			String chunkids = ChunkHelper.toString(chunkid);
 			boolean loadchunk = ChunkHelper.shouldLoad(chunkids, chunkLoadBehaviour);
-			offset += chunk.readChunkData(is);
+			offset += chunk.readChunkData(inputStream);
 			if (chunkids.equals(ChunkHelper.PLTE))
 				currentChunkGroup = ChunksList.CHUNK_GROUP_2_PLTE;
 			if (loadchunk)
-				addChunkToList(chunk);
+				parseChunkAndAddToList(chunk);
 			if (chunkids.equals(ChunkHelper.PLTE))
 				currentChunkGroup = ChunksList.CHUNK_GROUP_3_AFTERPLTE;
 		}
 		int idatLen = found ? clen : -1;
 		if (idatLen < 0)
 			throw new PngjInputException("first idat chunk not found!");
-		iIdatCstream = new PngIDatChunkInputStream(is, idatLen, offset);
+		iIdatCstream = new PngIDatChunkInputStream(inputStream, idatLen, offset);
 		idatIstream = new InflaterInputStream(iIdatCstream);
 	}
 
@@ -340,15 +358,6 @@ public class PngReader {
 	 */
 	protected void logWarn(String warn) {
 		System.err.println(warn);
-	}
-
-	/**
-	 * Dummy method
-	 * <p>
-	 * Since version 0.88 (Apr 2012) the ending chunks are read automatically, internally, after reading the last row.
-	 * This does nothing now, just kept for backward compatibily
-	 */
-	public void end() {
 	}
 
 	public ChunkLoadBehaviour getChunkLoadBehaviour() {
@@ -388,11 +397,31 @@ public class PngReader {
 	 */
 	public ImageLine readRow(int nrow) {
 		readRow(imgLine.scanline, nrow);
-		imgLine.filterUsed = FilterType.getByVal(rowbfilter[0]);
+		imgLine.setFilterUsed(FilterType.getByVal(rowbfilter[0]));
 		imgLine.setRown(nrow);
 		return imgLine;
 	}
 
+	/**
+	 * Like <code>readRow(int nrow)</code> but this accepts non consecutive rows.
+	 * <p>
+	 * If it's the current row, it will just return it. Elsewhere, it will try to read it.
+	 * This implementation only accepts  nrow greater or equal than current row, but
+	 * an extended class could implement some partial or full cache of lines.
+	 * <p>
+	 * This should not  not be mixed with calls to <code>readRow(int[] buffer, final int nrow)</code> 
+	 * @param nrow
+	 * @return
+	 */
+	public ImageLine getRow(int nrow) {
+		while(rowNum < nrow ) readRow(rowNum+1);
+		// now it should be positioned in the desired row
+		if(rowNum != nrow  || imgLine.getRown() != nrow) 
+			throw new PngjInputException("Invalid row: " + nrow);
+		return imgLine;
+	}
+
+	
 	/**
 	 * Reads a line and returns it as a int[] array.
 	 * <p>
@@ -445,12 +474,20 @@ public class PngReader {
 	}
 
 	/**
-	 * if true, input stream will be closed after ending read 
+	 * if true, input stream will be closed after ending read
 	 * <p>
 	 * default=true
 	 */
 	public void setShouldCloseStream(boolean shouldCloseStream) {
 		this.shouldCloseStream = shouldCloseStream;
+	}
+
+	/**
+	 * Normally this does nothing, but it can be used to force a premature closing
+	 */
+	public void end() {
+		if (currentChunkGroup < ChunksList.CHUNK_GROUP_6_END)
+			close();
 	}
 
 	/**
