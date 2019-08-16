42c0fca8140adf6e2147d371fe36ff4b0df509f2
Hernan Gonzalez <hjg.com.ar@gmail.com>
2019-08-16 11:19:11 -0300

    Use JUL (Java utility logging) for logging

--- src/test/java/ar/com/hjg/pngj/test/PngjTest.java
+++ src/test/java/ar/com/hjg/pngj/test/PngjTest.java
@@ -1,16 +1,22 @@
 package ar.com.hjg.pngj.test;
 
 import org.junit.After;
+import org.junit.BeforeClass;
 
 public class PngjTest {
 
-  /** change to false if you want to inspect the temporary files */
-  protected boolean clearTempFiles = true;
+    /** change to false if you want to inspect the temporary files */
+    protected boolean clearTempFiles = true;
 
-  @After
-  public void tearDown() {
-    if (clearTempFiles) {
-      TestSupport.cleanAll();
+    @BeforeClass
+    public static void setup() {
+        System.setProperty("java.util.logging.config.file", ClassLoader.getSystemResource("logging.properties").getPath());
     }
-  }
+    
+    @After
+    public void tearDown() {
+	if (clearTempFiles) {
+	    TestSupport.cleanAll();
+	}
+    }
 }
